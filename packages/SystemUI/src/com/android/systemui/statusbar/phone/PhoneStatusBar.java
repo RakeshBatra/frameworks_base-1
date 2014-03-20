e87d4702fd72187cd1dc8b786d0e4ffdd77fbff2
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-20 20:48:04 +0700

    Implement App circle sidebar [1/2]
    Change-Id: I99d9a9a9e9f9df99e999e999d9cac9999a999a9e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1385,6 +1385,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         addGestureAnywhereView();
+        addAppCircleSidebar();
 
         mAssistManager = SystemUIFactory.getInstance().createAssistManager(this, context);
 
